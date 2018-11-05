e5e14fc0ca5b0ce68ab35ac39770beabb12650dd
Cody Stuck <hellocodystuck@gmail.com>
2018-11-05 10:36:17 -0800

    restructured files

--- src/HTree.java
+++
@@ -1,8 +0,0 @@
-// File: HTree.java
-// Description: Contains the definition of the HTree class
-// Author: Cody Stuck
-
-public class HTree
-{
-
-}
--- src/HuffmanTree.java
+++ src/HuffmanTree.java
@@ -1,8 +1,8 @@
-// File: Huffman.java
+// File: HuffmanTree.java
 // Description: Contains the definition for the Huffman class, and the main function of the program
 // Author: Cody Stuck
 
-public class Huffman
+public class HuffmanTree
 {
 
 }
